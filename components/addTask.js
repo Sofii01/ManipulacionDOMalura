75695b44fd254b3ba5db7338dc2d556cce5a6d36
Sofia Britos <sofiibritos01@gmail.com>
2022-09-05 21:11:23 -0300

    Aula03 terminada

--- components/addTask.js
+++ components/addTask.js
@@ -2,58 +2,48 @@ import checkComplete from "./checkComplete.js";
 import deleteIcon from "./deleteIcon.js";
 
 export const addTask = (evento) => {
-    const list = document.querySelector("[data-list]");
-    const task = createTask(evento);
-    list.appendChild(task);
-
-}
-
-
-
-const createTask = (evento) => {
     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+    const list = document.querySelector("[data-list]");
     const input = document.querySelector("[data-form-input]");
     const calendar = document.querySelector("[data-form-date]");
+
     const value = input.value;
     const date = calendar.value;
     const dateFormat = moment(date).format('DD/MM/YYYY');
 
-    const task = document.createElement("li");
-    task.classList.add("card");
     input.value = ' ';
-    // backticks
-
+    calendar.value = '';
     const taskObj = {
         value,
         dateFormat
     }
 
+    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
     taskList.push(taskObj);
-    // si cerramos la pestaña los datos no persisten
-    // sessionStorage.setItem("tasks", JSON.stringify(taskObj)); 
-
     localStorage.setItem('tasks', JSON.stringify(taskList));
+
+    const task = createTask(taskObj);
+    list.appendChild(task);
+
+}
+
+export const createTask = ({ value, dateFormat }) => {
+    const task = document.createElement("li");
+    task.classList.add("card");
+
     const taskContent = document.createElement("div");
+
     const titleTask = document.createElement("span");
     titleTask.classList.add("task");
     titleTask.innerHTML = value;
-
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    // taskContent.appendChild(deleteIcon());
 
-    // const content = ` 
-    // <i class="fas fa-trash-alt trashIcon icon"></i>`
     const dateElement = document.createElement("span");
     dateElement.innerHTML = dateFormat;
-
     task.appendChild(taskContent);
     taskContent.appendChild(dateElement);
     task.appendChild(deleteIcon())
     return task;
-
-    //   task.innerHTML = content;
-
 }
 
